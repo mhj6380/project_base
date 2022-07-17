a6103c0b34d396edad66a7e4500d29ba6cb90c9e
mhj6380 <mhj6407@gmail.com>
2022-07-17 12:51:12 +0900

    Update crawling.py

--- crawling.py
+++ crawling.py
@@ -1,5 +1,5 @@
 import requests
-from beautifulsoup4 import BeautifulSoap
+from bs4 import BeautifulSoap
 
 url = 'https://ridibooks.com/category/new-releases/2200'
 response = requests.get(url)
@@ -8,6 +8,6 @@ html = response.text
 
 soap = BeautifulSoap(html, 'html.parser')
 
-bookservices = soap.select('.title_text')
+bookservices = soap.select('.title_text') 
 for no, book in enumerate(bookservices, 1): 
   print(no, book, text.strip()) 
